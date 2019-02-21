a1d2de520ad01efeb018b08f35d796a9ba2aba88
Ami Zou <ami.yx.zou@gmail.com>
2019-02-20 23:07:36 -0500

    new models

---
+++ src/app/PriorValueContextAdaptiveACEncodeVideoFile.java
@@ -0,0 +1,12 @@
+package app;
+
+import java.io.IOException;
+
+public class PriorValueContextAdaptiveACEncodeVideoFile {
+	
+	public static void main(String[] args) throws IOException {	
+		String input_file_name = "data/out.dat";
+		String output_file_name = "data/prior-value-context-adaptive-compressed-out.dat";
+		
+	}
+}
---
+++ src/app/PriorValuePixelModel.java
@@ -0,0 +1,25 @@
+package app;
+
+import ac.SourceModel;
+
+public class PriorValuePixelModel implements SourceModel<Integer> {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Integer get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double cdfLow(int index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
