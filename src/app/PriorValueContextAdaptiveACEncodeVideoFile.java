a14a9ddf18dfab4a02f97743066bf0e768e6bdff
Ami Zou <ami.yx.zou@gmail.com>
2019-02-21 00:14:02 -0500

    created model and encoder

--- src/app/PriorValueContextAdaptiveACEncodeVideoFile.java
+++ src/app/PriorValueContextAdaptiveACEncodeVideoFile.java
@@ -1,6 +1,12 @@
 package app;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+
+import ac.ArithmeticEncoder;
+import io.OutputStreamBitSink;
 
 public class PriorValueContextAdaptiveACEncodeVideoFile {
 	
@@ -8,5 +14,90 @@ public class PriorValueContextAdaptiveACEncodeVideoFile {
 		String input_file_name = "data/out.dat";
 		String output_file_name = "data/prior-value-context-adaptive-compressed-out.dat";
 		
+		int range_bit_width = 40;
+
+		System.out.println("Encoding video file: " + input_file_name);
+		System.out.println("Output compressed dat: " + output_file_name);
+		System.out.println("Range Register Bit Width: " + range_bit_width);
+
+		int num_pixels = (int) new File(input_file_name).length();
+				
+		Integer[] differences = new Integer[512]; //Possible values: from -255 to +255
+		for (int i=-255; i<=255; i++) {
+			differences[i+255] = i;
+			//System.out.println("differences[i] " + (i+255) + " is " + i);
+		}
+
+		// Create 64 * 64 = 4096 models for each pixel position in one frame. 
+		// Model chosen depends on value of pixel value in the prior frame.
+		
+		PriorValuePixelModel[] models = new PriorValuePixelModel[4096];
+		
+		for (int i=0; i<4096; i++) {
+			// Create new model with default count of 1 for all symbols
+			// TODO: another way???
+			models[i] = new PriorValuePixelModel(differences);
+		}
+
+		ArithmeticEncoder<Integer> encoder = new ArithmeticEncoder<Integer>(range_bit_width);
+
+		FileOutputStream fos = new FileOutputStream(output_file_name);
+		OutputStreamBitSink bit_sink = new OutputStreamBitSink(fos);
+
+		// First 4 bytes are the number of pixels encoded
+		bit_sink.write(num_pixels, 32);		
+
+		// Next byte is the width of the range registers
+		bit_sink.write(range_bit_width, 8);
+
+		// Now encode the input
+		FileInputStream fis = new FileInputStream(input_file_name);
+		
+		// Use model 0 as initial model + initiate the frame
+		PriorValuePixelModel model = models[0];
+		int[] lastFrame = new int[4096];
+		
+		// Get the first 4096 pixels as the base current_frame
+		for (int i=0; i<4096; i++) {
+			int next_pixel = fis.read();
+			
+			lastFrame[i] = next_pixel;
+			model = models[i];
+			
+			encoder.encode(next_pixel+255, model, bit_sink);
+		}
+
+		for (int i=4096; i<num_pixels; i++) {
+			int next_pixel = fis.read();
+			int absoluteIndex = i % 4096;
+			model = models[absoluteIndex];
+			
+			// Encoding and updating the difference
+			Integer difference = next_pixel - lastFrame[absoluteIndex];
+			difference += 255; //For handling negative numbers
+			encoder.encode(difference, model, bit_sink);
+			
+			// Update model used
+			model.updateCount(difference);
+			
+			// Update current frame
+			lastFrame[absoluteIndex] = next_pixel;
+			
+			// Set up next model based on symbol just encoded
+			// model = models[next_pixel];
+			
+		}
+		
+		
+		fis.close();
+
+		// Finish off by emitting the middle pattern 
+		// and padding to the next word
+		
+		encoder.emitMiddle(bit_sink);
+		bit_sink.padToWord();
+		fos.close();
+		
+		System.out.println("Done");
 	}
 }
--- src/app/PriorValuePixelModel.java
+++ src/app/PriorValuePixelModel.java
@@ -3,23 +3,76 @@ package app;
 import ac.SourceModel;
 
 public class PriorValuePixelModel implements SourceModel<Integer> {
+	
+	private Integer[] _differences;
+	private int[] _counts;
+	private int _total_count;
+	
+	public PriorValuePixelModel(Integer[] differences, int[] counts) {
+		assert differences != null;
+		assert differences.length > 1;
+		if (counts != null) {
+			assert differences.length == counts.length;			
+		} else {
+			counts = new int[differences.length];			
+			for (int i=0; i<counts.length; i++) {
+				counts[i] = 1; //TODO: or 0 here?
+			}
+			//System.out.println("Created a model with counts length " + counts.length);
+		}
+
+		_total_count = 0;
+		for (int i=0; i<differences.length; i++) {
+			assert differences[i] != null;
+			assert counts[i] >= 0;
+			_total_count += counts[i];
+		}
+		
+		_differences = differences.clone();
+		_counts = counts.clone();
+	}
+	
+	public PriorValuePixelModel(Integer[] differences) {
+		this(differences, null);
+	}
+	
+	public void updateCount(int index) {
+		_counts[index]++;
+		_total_count++;
+	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return _differences.length;
 	}
 
 	@Override
 	public Integer get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		assert index >= 0 && index < size();
+		
+		return _differences[index];
 	}
 
 	@Override
 	public double cdfLow(int index) {
-		// TODO Auto-generated method stub
-		return 0;
+		assert index >= 0 && index < size();
+
+		int cumulative_count = 0;
+
+		for (int i=0; i < index; i++) {
+			cumulative_count += _counts[i];
+		}
+		
+		return (1.0 * cumulative_count) / (1.0 * _total_count);
+	}
+	
+	public int lookup(Integer diff) {
+		for (int i=0; i<size(); i++) {
+			if (get(i).equals(diff+255)) {
+				return i;
+			}
+		}
+		throw new RuntimeException("Symbol not in source model");
 	}
 
 }
