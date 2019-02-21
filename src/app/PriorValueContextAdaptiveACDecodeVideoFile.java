f36a5a78d52a975f855c62577d8ff4c7d5c9423f
Ami Zou <ami.yx.zou@gmail.com>
2019-02-21 13:31:31 -0500

    decoder

--- src/app/PriorValueContextAdaptiveACDecodeVideoFile.java
+++ src/app/PriorValueContextAdaptiveACDecodeVideoFile.java
@@ -1,5 +1,101 @@
 package app;
 
-public class PriorValueContextAdaptiveACDecodeVideoFile {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import ac.ArithmeticDecoder;
+import io.InputStreamBitSource;
+import io.InsufficientBitsLeftException;
+
+public class PriorValueContextAdaptiveACDecodeVideoFile {
+	public static void main(String[] args) throws InsufficientBitsLeftException, IOException {
+		String input_file_name = "data/prior-value-context-adaptive-compressed-out.dat";
+		String output_file_name = "data/prior-value-context-adaptive-reuncompressed-out.dat";
+
+		FileInputStream fis = new FileInputStream(input_file_name);
+
+		InputStreamBitSource bit_source = new InputStreamBitSource(fis);
+
+		Integer[] differences = new Integer[511]; //Possible values: from -255 to +255
+		for (int i=0; i<differences.length; i++) {
+			differences[i] = i-255;
+			//System.out.println("differences[i] " + i + " is " + (i-255));
+		}
+		
+		PriorValuePixelModel[] models = new PriorValuePixelModel[4096];
+		
+		for (int i=0; i<4096; i++) {
+			// Create new model with default count of 1 for all symbols
+			// TODO: another way???
+			models[i] = new PriorValuePixelModel(differences);
+		}
+		
+		// Read in number of symbols encoded
+
+		int num_pixels = bit_source.next(32);
+
+		// Read in range bit width and setup the decoder
+
+		int range_bit_width = bit_source.next(8);
+		ArithmeticDecoder<Integer> decoder = new ArithmeticDecoder<Integer>(range_bit_width);
+
+		// Decode and produce output.
+		
+		System.out.println("Uncompressing dat file: " + input_file_name);
+		System.out.println("Output dat file: " + output_file_name);
+		System.out.println("Range Register Bit Width: " + range_bit_width);
+		System.out.println("Number of decoded pixels: " + num_pixels);
+		
+		FileOutputStream fos = new FileOutputStream(output_file_name);
+
+		// Use model 0 as initial model.
+		PriorValuePixelModel model = models[0];
+		int[] lastFrame = new int[4096];
+		
+		// Get the first 4096 pixels as the base current_frame
+		for (int i=0; i<4096; i++) {
+			int next_pixel = decoder.decode(model, bit_source);
+			System.out.println("Decoded 1st frame pixel " + i +" with value" + next_pixel);
+			fos.write(next_pixel);
+			
+			lastFrame[i] = next_pixel;
+			model = models[i];
+			
+			//encoder.encode(next_pixel, model, bit_sink);
+			
+			//System.out.println("Save the 1st frame " + i + ": " + next_pixel);
+		}
+
+		for (int i=4096; i<num_pixels; i++) {
+			int absoluteIndex = i % 4096;
+			model = models[absoluteIndex];
+			
+			int next_diff = decoder.decode(model, bit_source);
+						
+			// Decoding - getting the actual pixel value + and updating the difference
+			Integer pixel = next_diff + lastFrame[absoluteIndex];
+			
+			fos.write(pixel);
+			System.out.println("Decoded following pixel: " + i +" with value " + pixel);
+			//System.out.println("The current pixel difference is " + difference);
+			//difference += 255; //For handling negative numbers
+			//encoder.encode(difference, model, bit_sink);
+			
+			// Update model used
+			model.updateCount(next_diff); //TODO: pixel or next_diff here?
+			
+			// Update current frame
+			lastFrame[absoluteIndex] = pixel;
+			
+			// Set up next model based on symbol just encoded
+			// model = models[next_pixel];
+			
+		}
+
+		System.out.println("Done.");
+		fos.flush();
+		fos.close();
+		fis.close();
+	}
 }
